e9bb6701b7abad31ef6469cccc12516e1019a072
Maximilian Stalzer <101569905+maxstalzer@users.noreply.github.com>
2026-02-21 13:48:37 +0100

    add week 3 ddpm implementation

--- Week 3/ddpm.py
+++ Week 3/ddpm.py
@@ -12,16 +12,6 @@ class DDPM(nn.Module):
     def __init__(self, network, beta_1=1e-4, beta_T=2e-2, T=100):
         """
         Initialize a DDPM model.
-
-        Parameters:
-        network: [nn.Module]
-            The network to use for the diffusion process.
-        beta_1: [float]
-            The noise at the first step of the diffusion process.
-        beta_T: [float]
-            The noise at the last step of the diffusion process.
-        T: [int]
-            The number of steps in the diffusion process.
         """
         super(DDPM, self).__init__()
         self.network = network
@@ -36,51 +26,59 @@ class DDPM(nn.Module):
     def negative_elbo(self, x):
         """
         Evaluate the DDPM negative ELBO on a batch of data.
-
-        Parameters:
-        x: [torch.Tensor]
-            A batch of data (x) of dimension `(batch_size, *)`.
-        Returns:
-        [torch.Tensor]
-            The negative ELBO of the batch of dimension `(batch_size,)`.
         """
+        batch_size = x.shape[0]
 
-        ### Implement Algorithm 1 here ###
-        neg_elbo = 0
+        # Sample a random timestep t uniformly for each sample in the batch
+        t = torch.randint(0, self.T, (batch_size,), device=x.device)
+
+        # Sample noise epsilon ~ N(0, I)
+        epsilon = torch.randn_like(x)
+
+        # Retrieve the cumulative product of alphas (alpha_bar) for the sampled timesteps
+        alpha_bar_t = self.alpha_cumprod[t].unsqueeze(1)
+
+        # Forward Process: Compute x_t (the noisy data)
+        x_t = torch.sqrt(alpha_bar_t) * x + torch.sqrt(1 - alpha_bar_t) * epsilon
+
+        # Format t for the network: shape (batch_size, 1), scaled by T
+        t_input = (t.float() / self.T).unsqueeze(1)
+        
+        # Predict the noise using the neural network
+        epsilon_theta = self.network(x_t, t_input)
+
+        # Calculate the squared error ||epsilon - epsilon_theta||^2
+        neg_elbo = torch.sum((epsilon - epsilon_theta) ** 2, dim=1)
 
         return neg_elbo
 
     def sample(self, shape):
         """
         Sample from the model.
-
-        Parameters:
-        shape: [tuple]
-            The shape of the samples to generate.
-        Returns:
-        [torch.Tensor]
-            The generated samples.
         """
         # Sample x_t for t=T (i.e., Gaussian noise)
         x_t = torch.randn(shape).to(self.alpha.device)
 
         # Sample x_t given x_{t+1} until x_0 is sampled
         for t in range(self.T-1, -1, -1):
-            ### Implement the remaining of Algorithm 2 here ###
-            pass
+            # Create timestep tensor and scale it exactly as done in training
+            t_tensor = torch.full((shape[0], 1), t, dtype=torch.float32, device=x_t.device)
+            t_input = t_tensor / self.T
+            
+            epsilon_theta = self.network(x_t, t_input)
+            
+            if t > 0:
+                z = torch.randn_like(x_t)
+            else:
+                z = torch.zeros_like(x_t)
+
+            x_t = (1/torch.sqrt(self.alpha[t])) * (x_t - (1 - self.alpha[t])/torch.sqrt(1 - self.alpha_cumprod[t])*epsilon_theta) + torch.sqrt(self.beta[t])*z
 
         return x_t
 
     def loss(self, x):
         """
         Evaluate the DDPM loss on a batch of data.
-
-        Parameters:
-        x: [torch.Tensor]
-            A batch of data (x) of dimension `(batch_size, *)`.
-        Returns:
-        [torch.Tensor]
-            The loss for the batch.
         """
         return self.negative_elbo(x).mean()
 
@@ -88,18 +86,6 @@ class DDPM(nn.Module):
 def train(model, optimizer, data_loader, epochs, device):
     """
     Train a Flow model.
-
-    Parameters:
-    model: [Flow]
-       The model to train.
-    optimizer: [torch.optim.Optimizer]
-         The optimizer to use for training.
-    data_loader: [torch.utils.data.DataLoader]
-            The data loader to use for training.
-    epochs: [int]
-        Number of epochs to train for.
-    device: [torch.device]
-        The device to use for training.
     """
     model.train()
 
@@ -118,20 +104,14 @@ def train(model, optimizer, data_loader, epochs, device):
             optimizer.step()
 
             # Update progress bar
-            progress_bar.set_postfix(loss=f"⠀{loss.item():12.4f}", epoch=f"{epoch+1}/{epochs}")
+            progress_bar.set_postfix(loss=f" {loss.item():12.4f}", epoch=f"{epoch+1}/{epochs}")
             progress_bar.update()
 
 
 class FcNetwork(nn.Module):
     def __init__(self, input_dim, num_hidden):
         """
-        Initialize a fully connected network for the DDPM, where the forward function also take time as an argument.
-        
-        parameters:
-        input_dim: [int]
-            The dimension of the input data.
-        num_hidden: [int]
-            The number of hidden units in the network.
+        Initialize a fully connected network for the DDPM.
         """
         super(FcNetwork, self).__init__()
         self.network = nn.Sequential(nn.Linear(input_dim+1, num_hidden), nn.ReLU(), 
@@ -141,12 +121,6 @@ class FcNetwork(nn.Module):
     def forward(self, x, t):
         """"
         Forward function for the network.
-        
-        parameters:
-        x: [torch.Tensor]
-            The input data of dimension `(batch_size, input_dim)`
-        t: [torch.Tensor]
-            The time steps to use for the forward pass of dimension `(batch_size, 1)`
         """
         x_t_cat = torch.cat([x, t], dim=1)
         return self.network(x_t_cat)
@@ -157,12 +131,13 @@ if __name__ == "__main__":
     from torchvision import datasets, transforms
     from torchvision.utils import save_image
     import ToyData
-
-    # Parse arguments
     import argparse
+    import matplotlib.pyplot as plt
+    import numpy as np
+
     parser = argparse.ArgumentParser()
     parser.add_argument('mode', type=str, default='train', choices=['train', 'sample', 'test'], help='what to do when running the script (default: %(default)s)')
-    parser.add_argument('--data', type=str, default='tg', choices=['tg', 'cb', 'mnist'], help='dataset to use {tg: two Gaussians, cb: chequerboard} (default: %(default)s)')
+    parser.add_argument('--data', type=str, default='tg', choices=['tg', 'cb', 'mnist'], help='dataset to use {tg: two Gaussians, cb: chequerboard, mnist: MNIST digits} (default: %(default)s)')
     parser.add_argument('--model', type=str, default='model.pt', help='file to save model to or load model from (default: %(default)s)')
     parser.add_argument('--samples', type=str, default='samples.png', help='file to save samples in (default: %(default)s)')
     parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda', 'mps'], help='torch device (default: %(default)s)')
@@ -175,62 +150,81 @@ if __name__ == "__main__":
     for key, value in sorted(vars(args).items()):
         print(key, '=', value)
 
-    # Generate the data
-    n_data = 10000000
-    toy = {'tg': ToyData.TwoGaussians, 'cb': ToyData.Chequerboard}[args.data]()
-    transform = lambda x: (x-0.5)*2.0
-    train_loader = torch.utils.data.DataLoader(transform(toy().sample((n_data,))), batch_size=args.batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(transform(toy().sample((n_data,))), batch_size=args.batch_size, shuffle=True)
+    # 1. GENERATE THE DATA
+    if args.data == 'mnist':
+        # Warn user if they are using default batch size for MNIST
+        if args.batch_size == 10000:
+            print("\nWARNING: Batch size 10000 is too large for MNIST and may cause an Out Of Memory error.")
+            print("Consider running with: --batch-size 128\n")
 
-    # Get the dimension of the dataset
-    D = next(iter(train_loader)).shape[1]
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x + torch.rand(x.shape) / 255),
+            transforms.Lambda(lambda x: (x - 0.5) * 2.0),
+            transforms.Lambda(lambda x: x.flatten())
+        ])
+        
+        train_data = datasets.MNIST('data/', train=True, download=True, transform=transform)
+        train_loader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+        
+        D = 784             # 28x28 flattened
+        num_hidden = 512    # Wider network for higher dimensional data
+    else:
+        n_data = 10000000
+        toy = {'tg': ToyData.TwoGaussians, 'cb': ToyData.Chequerboard}[args.data]()
+        transform = lambda x: (x-0.5)*2.0
+        train_loader = torch.utils.data.DataLoader(transform(toy().sample((n_data,))), batch_size=args.batch_size, shuffle=True)
+        
+        D = next(iter(train_loader)).shape[1]
+        num_hidden = 64     # Smaller network is sufficient for toy data
 
-    # Define the network
-    num_hidden = 64
+    # 2. DEFINE THE NETWORK AND MODEL
     network = FcNetwork(D, num_hidden)
-
-    # Set the number of steps in the diffusion process
     T = 1000
-
-    # Define model
     model = DDPM(network, T=T).to(args.device)
 
-    # Choose mode to run
+    # 3. CHOOSE MODE TO RUN
     if args.mode == 'train':
-        # Define optimizer
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-
-        # Train model
         train(model, optimizer, train_loader, args.epochs, args.device)
-
-        # Save model
         torch.save(model.state_dict(), args.model)
 
     elif args.mode == 'sample':
-        import matplotlib.pyplot as plt
-        import numpy as np
-
-        # Load the model
         model.load_state_dict(torch.load(args.model, map_location=torch.device(args.device)))
-
-        # Generate samples
         model.eval()
-        with torch.no_grad():
-            samples = (model.sample((10000,D))).cpu() 
 
-        # Transform the samples back to the original space
-        samples = samples /2 + 0.5
+        if args.data == 'mnist':
+            # Generate 64 samples to create an 8x8 grid
+            with torch.no_grad():
+                samples = model.sample((64, D)).cpu()
+            
+            # Transform the samples back to [0, 1] range
+            samples = samples / 2 + 0.5
+            
+            # Reshape flattened vectors back into 28x28 image matrices
+            samples = samples.view(-1, 1, 28, 28)
+            
+            # Save using torchvision's save_image functionality
+            save_image(samples, args.samples, nrow=8)
+            print(f"Saved MNIST samples grid to {args.samples}")
 
-        # Plot the density of the toy data and the model samples
-        coordinates = [[[x,y] for x in np.linspace(*toy.xlim, 1000)] for y in np.linspace(*toy.ylim, 1000)]
-        prob = torch.exp(toy().log_prob(torch.tensor(coordinates)))
+        else:
+            with torch.no_grad():
+                samples = (model.sample((10000, D))).cpu() 
 
-        fig, ax = plt.subplots(1, 1, figsize=(7, 5))
-        im = ax.imshow(prob, extent=[toy.xlim[0], toy.xlim[1], toy.ylim[0], toy.ylim[1]], origin='lower', cmap='YlOrRd')
-        ax.scatter(samples[:, 0], samples[:, 1], s=1, c='black', alpha=0.5)
-        ax.set_xlim(toy.xlim)
-        ax.set_ylim(toy.ylim)
-        ax.set_aspect('equal')
-        fig.colorbar(im)
-        plt.savefig(args.samples)
-        plt.close()
+            # Transform the samples back to the original space
+            samples = samples / 2 + 0.5
+
+            # Plot the density of the toy data and the model samples
+            coordinates = [[[x,y] for x in np.linspace(*toy.xlim, 1000)] for y in np.linspace(*toy.ylim, 1000)]
+            prob = torch.exp(toy().log_prob(torch.tensor(coordinates)))
+
+            fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+            im = ax.imshow(prob, extent=[toy.xlim[0], toy.xlim[1], toy.ylim[0], toy.ylim[1]], origin='lower', cmap='YlOrRd')
+            ax.scatter(samples[:, 0], samples[:, 1], s=1, c='black', alpha=0.5)
+            ax.set_xlim(toy.xlim)
+            ax.set_ylim(toy.ylim)
+            ax.set_aspect('equal')
+            fig.colorbar(im)
+            plt.savefig(args.samples)
+            plt.close()
